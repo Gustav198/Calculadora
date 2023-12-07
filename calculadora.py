f863f1dea7a9940d45d2e4876c752abf723c0064
Gustavo Ortega <gustavo.ortega.1985@gmail.com>
2023-12-07 00:37:41 -0300

    Changes in line 34

---
+++ calculadora.py
@@ -0,0 +1,34 @@
+print("Bienvenidos a la calculadora")
+print("para salir escribe Salir")
+print("las operaciones son suma, multi, div y resta")
+
+
+resultado = ("")
+while True:
+    if not resultado:
+        resultado = input("ingrese numero: ")
+        if resultado.lower() == "salir":
+            break
+
+    resultado = int(resultado)
+
+    op = input("ingresa operacion :")
+    if op.lower() == ("salir"):
+        break
+    n2 = input("ingresa siguiente numero :")
+    if n2.lower() == "salir":
+        break
+    n2 = int(n2)
+
+    if op.lower() == ("suma"):
+        resultado += n2
+    elif op.lower() == ("resta"):
+        resultado -= n2
+    elif op.lower() == ("multi"):
+        resultado *= n2
+    elif op.lower() == ("div"):
+        resultado /= n2
+    else:
+        print("Operacion no valida")
+        break
+    print(f"el resultado es {resultado}")
